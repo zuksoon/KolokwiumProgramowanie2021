41294200f32ca90c16510f1ce88611f24f402778
Pawel Zukowicz <junzuk2000@gmail.com>
2021-06-16 13:07:36 +0200

    final

--- Kolokwiumw61919/Kolokwiumw61919/MainWindow.xaml.cs
+++ Kolokwiumw61919/Kolokwiumw61919/MainWindow.xaml.cs
@@ -24,5 +24,98 @@ namespace Kolokwiumw61919
         {
             InitializeComponent();
         }
+
+        private void BtnNettoNaBrutto_Click(object sender, RoutedEventArgs e)
+        {
+            
+            double kwotaNetto = 0;
+            double SamaKwotaBrutto = 0;
+            try
+            {
+                kwotaNetto = double.Parse(TextBoxNetto1.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wprowadzono błędne dane !");
+            }
+
+            if (ComboBoxNettoNaBrutto.SelectedIndex < 0)
+            {
+                MessageBox.Show("Nie wybrano kwoty Vat !");
+            }
+            else
+            {
+                if(ComboBoxNettoNaBrutto.SelectedIndex == 0)
+                {
+                    SamaKwotaBrutto = (kwotaNetto * 23)/100;
+                    TextBoxBrutto1.Text = (kwotaNetto + SamaKwotaBrutto).ToString();
+                }
+                if (ComboBoxNettoNaBrutto.SelectedIndex == 1)
+                {
+                    SamaKwotaBrutto = (kwotaNetto * 8) / 100;
+                    TextBoxBrutto1.Text = (kwotaNetto + SamaKwotaBrutto).ToString();
+                }
+                if (ComboBoxNettoNaBrutto.SelectedIndex == 2)
+                {
+                    SamaKwotaBrutto = (kwotaNetto * 7) / 100;
+                    TextBoxBrutto1.Text = (kwotaNetto + SamaKwotaBrutto).ToString();
+                }
+                if (ComboBoxNettoNaBrutto.SelectedIndex == 3)
+                {
+                    SamaKwotaBrutto = (kwotaNetto * 5) / 100;
+                    TextBoxBrutto1.Text = (kwotaNetto + SamaKwotaBrutto).ToString();
+                }
+                if (ComboBoxNettoNaBrutto.SelectedIndex == 4)
+                {
+                    TextBoxBrutto1.Text = kwotaNetto.ToString();
+                }
+            }
+        }
+
+        private void BtnBruttoNaNetto_Click(object sender, RoutedEventArgs e)
+        {
+            double kwotaBrutto = 0;
+            double SamaKwotaNetto = 0;
+            try
+            {
+                kwotaBrutto = double.Parse(TextBoxBrutto2.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wprowadzono błędne dane !");
+            }
+
+            if (ComboBoxBruttoNaNetto.SelectedIndex < 0)
+            {
+                MessageBox.Show("Nie wybrano kwoty Vat !");
+            }
+            else
+            {
+                if (ComboBoxBruttoNaNetto.SelectedIndex == 0)
+                {
+                    SamaKwotaNetto = (kwotaBrutto * 23) / 100;
+                    TextBoxNetto2.Text = (kwotaBrutto - SamaKwotaNetto).ToString();
+                }
+                if (ComboBoxBruttoNaNetto.SelectedIndex == 1)
+                {
+                    SamaKwotaNetto = (kwotaBrutto * 8) / 100;
+                    TextBoxNetto2.Text = (kwotaBrutto - SamaKwotaNetto).ToString();
+                }
+                if (ComboBoxBruttoNaNetto.SelectedIndex == 2)
+                {
+                    SamaKwotaNetto = (kwotaBrutto * 7) / 100;
+                    TextBoxNetto2.Text = (kwotaBrutto - SamaKwotaNetto).ToString();
+                }
+                if (ComboBoxBruttoNaNetto.SelectedIndex == 3)
+                {
+                    SamaKwotaNetto = (kwotaBrutto * 5) / 100;
+                    TextBoxNetto2.Text = (kwotaBrutto - SamaKwotaNetto).ToString();
+                }
+                if (ComboBoxBruttoNaNetto.SelectedIndex == 4)
+                {
+                    TextBoxNetto2.Text = kwotaBrutto.ToString();
+                }
+            }
+        }
     }
 }
